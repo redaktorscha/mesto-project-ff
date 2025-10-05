a5502175349bb579080f69bad0c8cd2c043995d6
Masha <nasonkina@yandex.ru>
2025-10-05 14:18:25 +0300

    fix cards import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import "./pages/index.css";
+import { initialCards } from "./components/cards.js";
+
+// В файле index.js должны остаться:
+// объявления и инициализация глобальных констант и переменных с DOM-элементами страницы;
+// обработчики событий (при открытии и закрытии попапов; при отправке форм; обработчик, открывающий попап при клике по изображению карточки);
+// вызовы других функций, подключённых из созданных модулей, которым нужно будет передавать объявленные здесь переменные и обработчики.
 
 // Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content.querySelector(".places__item");
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".places__item");
 
 // DOM узлы
 const placesWrap = document.querySelector(".places__list");
